11918cf6d1d2e1ae09d2dca5b50dbe66cf86dc9c
David Luecke <daff@neyeon.com>
2016-04-22 16:03:43 -0700

    3.0.0-pre.1

---
+++ dist/global/can-list.js
@@ -0,0 +1,394 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-list@3.0.0-pre.0#can-list*/
+define('can-list', function (require, exports, module) {
+    require('can-event');
+    var Map = require('can-map');
+    var bubble = require('can-map/bubble');
+    var mapHelpers = require('can-map/map-helpers');
+    var canBatch = require('can-event/batch/batch');
+    var ObserveInfo = require('can-observe-info');
+    var CID = require('can-util/js/cid/cid');
+    var isPromise = require('can-util/js/is-promise/is-promise');
+    var makeArray = require('can-util/js/make-array/make-array');
+    var assign = require('can-util/js/assign/assign');
+    var types = require('can-util/js/types/types');
+    var each = require('can-util/js/each/each');
+    var splice = [].splice, spliceRemovesProps = function () {
+            var obj = {
+                0: 'a',
+                length: 1
+            };
+            splice.call(obj, 0, 1);
+            return !obj[0];
+        }();
+    var List = Map.extend({ Map: Map }, {
+            setup: function (instances, options) {
+                this.length = 0;
+                CID(this, '.map');
+                this._setupComputedProperties();
+                instances = instances || [];
+                var teardownMapping;
+                if (isPromise(instances)) {
+                    this.replace(instances);
+                } else {
+                    teardownMapping = instances.length && mapHelpers.addToMap(instances, this);
+                    this.push.apply(this, makeArray(instances || []));
+                }
+                if (teardownMapping) {
+                    teardownMapping();
+                }
+                assign(this, options);
+            },
+            _triggerChange: function (attr, how, newVal, oldVal) {
+                Map.prototype._triggerChange.apply(this, arguments);
+                var index = +attr;
+                if (!~('' + attr).indexOf('.') && !isNaN(index)) {
+                    if (how === 'add') {
+                        canBatch.trigger.call(this, how, [
+                            newVal,
+                            index
+                        ]);
+                        canBatch.trigger.call(this, 'length', [this.length]);
+                    } else if (how === 'remove') {
+                        canBatch.trigger.call(this, how, [
+                            oldVal,
+                            index
+                        ]);
+                        canBatch.trigger.call(this, 'length', [this.length]);
+                    } else {
+                        canBatch.trigger.call(this, how, [
+                            newVal,
+                            index
+                        ]);
+                    }
+                }
+            },
+            ___get: function (attr) {
+                if (attr) {
+                    if (this[attr] && this[attr].isComputed && typeof this.constructor.prototype[attr] === 'function') {
+                        return this[attr]();
+                    } else {
+                        return this[attr];
+                    }
+                } else {
+                    return this;
+                }
+            },
+            __set: function (prop, value, current) {
+                prop = isNaN(+prop) || prop % 1 ? prop : +prop;
+                if (typeof prop === 'number' && prop > this.length - 1) {
+                    var newArr = new Array(prop + 1 - this.length);
+                    newArr[newArr.length - 1] = value;
+                    this.push.apply(this, newArr);
+                    return newArr;
+                }
+                return Map.prototype.__set.call(this, '' + prop, value, current);
+            },
+            ___set: function (attr, val) {
+                this[attr] = val;
+                if (+attr >= this.length) {
+                    this.length = +attr + 1;
+                }
+            },
+            __remove: function (prop, current) {
+                if (isNaN(+prop)) {
+                    delete this[prop];
+                    this._triggerChange(prop, 'remove', undefined, current);
+                } else {
+                    this.splice(prop, 1);
+                }
+            },
+            _each: function (callback) {
+                var data = this.___get();
+                for (var i = 0; i < data.length; i++) {
+                    callback(data[i], i);
+                }
+            },
+            serialize: function () {
+                return mapHelpers.serialize(this, 'serialize', []);
+            },
+            splice: function (index, howMany) {
+                var args = makeArray(arguments), added = [], i, len, listIndex, allSame = args.length > 2;
+                index = index || 0;
+                for (i = 0, len = args.length - 2; i < len; i++) {
+                    listIndex = i + 2;
+                    args[listIndex] = this.__type(args[listIndex], listIndex);
+                    added.push(args[listIndex]);
+                    if (this[i + index] !== args[listIndex]) {
+                        allSame = false;
+                    }
+                }
+                if (allSame && this.length <= added.length) {
+                    return added;
+                }
+                if (howMany === undefined) {
+                    howMany = args[1] = this.length - index;
+                }
+                var removed = splice.apply(this, args);
+                if (!spliceRemovesProps) {
+                    for (i = this.length; i < removed.length + this.length; i++) {
+                        delete this[i];
+                    }
+                }
+                canBatch.start();
+                if (howMany > 0) {
+                    bubble.removeMany(this, removed);
+                    this._triggerChange('' + index, 'remove', undefined, removed);
+                }
+                if (args.length > 2) {
+                    bubble.addMany(this, added);
+                    this._triggerChange('' + index, 'add', added, removed);
+                }
+                canBatch.stop();
+                return removed;
+            },
+            _getAttrs: function () {
+                return mapHelpers.serialize(this, 'attr', []);
+            },
+            _setAttrs: function (items, remove) {
+                items = makeArray(items);
+                canBatch.start();
+                this._updateAttrs(items, remove);
+                canBatch.stop();
+            },
+            _updateAttrs: function (items, remove) {
+                var len = Math.min(items.length, this.length);
+                for (var prop = 0; prop < len; prop++) {
+                    var curVal = this[prop], newVal = items[prop];
+                    if (types.isMapLike(curVal) && mapHelpers.canMakeObserve(newVal)) {
+                        curVal.attr(newVal, remove);
+                    } else if (curVal !== newVal) {
+                        this._set(prop + '', newVal);
+                    } else {
+                    }
+                }
+                if (items.length > this.length) {
+                    this.push.apply(this, items.slice(this.length));
+                } else if (items.length < this.length && remove) {
+                    this.splice(items.length);
+                }
+            }
+        }), getArgs = function (args) {
+            return args[0] && Array.isArray(args[0]) ? args[0] : makeArray(args);
+        };
+    each({
+        push: 'length',
+        unshift: 0
+    }, function (where, name) {
+        var orig = [][name];
+        List.prototype[name] = function () {
+            var args = [], len = where ? this.length : 0, i = arguments.length, res, val;
+            while (i--) {
+                val = arguments[i];
+                args[i] = bubble.set(this, i, this.__type(val, i));
+            }
+            res = orig.apply(this, args);
+            if (!this.comparator || args.length) {
+                this._triggerChange('' + len, 'add', args, undefined);
+            }
+            return res;
+        };
+    });
+    each({
+        pop: 'length',
+        shift: 0
+    }, function (where, name) {
+        List.prototype[name] = function () {
+            if (!this.length) {
+                return undefined;
+            }
+            var args = getArgs(arguments), len = where && this.length ? this.length - 1 : 0;
+            var res = [][name].apply(this, args);
+            this._triggerChange('' + len, 'remove', undefined, [res]);
+            if (res && res.removeEventListener) {
+                bubble.remove(this, res);
+            }
+            return res;
+        };
+    });
+    assign(List.prototype, {
+        indexOf: function (item, fromIndex) {
+            ObserveInfo.observe(this, 'length');
+            for (var i = fromIndex || 0, len = this.length; i < len; i++) {
+                if (this.attr(i) === item) {
+                    return i;
+                }
+            }
+            return -1;
+        },
+        join: function () {
+            ObserveInfo.observe(this, 'length');
+            return [].join.apply(this, arguments);
+        },
+        reverse: function () {
+            var list = [].reverse.call(makeArray(this));
+            return this.replace(list);
+        },
+        slice: function () {
+            ObserveInfo.observe(this, 'length');
+            var temp = Array.prototype.slice.apply(this, arguments);
+            return new this.constructor(temp);
+        },
+        concat: function () {
+            var args = [];
+            each(makeArray(arguments), function (arg, i) {
+                args[i] = arg instanceof List ? arg.serialize() : arg;
+            });
+            return new this.constructor(Array.prototype.concat.apply(this.serialize(), args));
+        },
+        forEach: function (cb, thisarg) {
+            var item;
+            for (var i = 0, len = this.attr('length'); i < len; i++) {
+                item = this.attr(i);
+                if (cb.call(thisarg || item, item, i, this) === false) {
+                    break;
+                }
+            }
+            return this;
+        },
+        replace: function (newList) {
+            if (isPromise(newList)) {
+                if (this._promise) {
+                    this._promise.__isCurrentPromise = false;
+                }
+                var promise = this._promise = newList;
+                promise.__isCurrentPromise = true;
+                var self = this;
+                newList.then(function (newList) {
+                    if (promise.__isCurrentPromise) {
+                        self.replace(newList);
+                    }
+                });
+            } else {
+                this.splice.apply(this, [
+                    0,
+                    this.length
+                ].concat(makeArray(newList || [])));
+            }
+            return this;
+        },
+        filter: function (callback, thisArg) {
+            var filteredList = new this.constructor(), self = this, filtered;
+            this.each(function (item, index, list) {
+                filtered = callback.call(thisArg | self, item, index, self);
+                if (filtered) {
+                    filteredList.push(item);
+                }
+            });
+            return filteredList;
+        },
+        map: function (callback, thisArg) {
+            var filteredList = new List(), self = this;
+            this.each(function (item, index, list) {
+                var mapped = callback.call(thisArg | self, item, index, self);
+                filteredList.push(mapped);
+            });
+            return filteredList;
+        }
+    });
+    types.isListLike = function (obj) {
+        return obj instanceof List;
+    };
+    var oldType = Map.prototype.__type;
+    Map.prototype.__type = function (value, prop) {
+        if (typeof value === 'object' && Array.isArray(value)) {
+            var cached = mapHelpers.getMapFromObject(value);
+            if (cached) {
+                return cached;
+            }
+            return new List(value);
+        }
+        return oldType.apply(this, arguments);
+    };
+    var oldSetup = Map.setup;
+    Map.setup = function () {
+        oldSetup.apply(this, arguments);
+        if (!(this.prototype instanceof List)) {
+            this.List = Map.List.extend({ Map: this }, {});
+        }
+    };
+    List.prototype.each = List.prototype.forEach;
+    Map.List = List;
+    module.exports = List;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
